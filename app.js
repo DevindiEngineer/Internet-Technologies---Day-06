cd76d73d012aef171a79b5a8bb9a8f45d35ae014
DevindiEngineer <devindicampus@gmail.com>
2026-07-04 10:19:18 +0530

    advanced part object structure

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 class Customer {
     name;
     age;
@@ -15,5 +16,104 @@ console.log(customer1);
 
 //===================================================
 
-let customer2 = { name: "Devindi", age: 20, address: "Colombo" };
+let customer2 = {
+    first_name: "Devindi",
+    country: "Colombo"
+};
 console.log(customer2);
+
+//===================================================
+
+let customer3 = {
+    name: "Ruw",
+    age: 21,
+    address: "Kandy",
+    salary:100000
+};
+console.log(customer3.name);
+console.log(customer3.age);
+console.log(customer3.address);
+console.log(customer3.salary);
+
+//====================================================
+
+let shop = {
+    name: "WIcky",
+    items: ["soap", "Bottle", "Box"]
+};
+console.log(shop);
+//or 
+console.log(shop.name);
+console.log(shop.items);
+//item by
+console.log(shop.items[0]);
+console.log(shop.items[1]);
+console.log(shop.items[2]);
+
+//==============================================
+
+let superM = {
+    cudID : 1001,
+    customer: {
+        name: "kanthi",
+        age: 60,
+        city: "Colombo"
+    }
+}
+console.log(superM.customer.name);*/
+
+//===============================================
+
+let superM = {
+    cusID: 1001,
+    items: [
+        {
+            id: 1,
+            name: "milk",
+            price: 1000
+        },
+        {
+            id: 2,
+            name: "Wine",
+            price: 9000
+        },
+        {
+            id: 3,
+            name: "Rice",
+            price: 740
+        }
+    ],
+    customer: {
+        name: "kanthi",
+        age: 60,
+        city: "Colombo"
+    }
+}
+
+console.log(superM);
+
+console.log(superM.cusID);
+console.log(superM.items);
+console.log(superM.customer);
+
+console.log(superM.customer.name);
+console.log(superM.customer.age);
+console.log(superM.customer.city);
+
+console.log(superM.items[0]);
+console.log(superM.items[1]);
+console.log(superM.items[2]);
+
+console.log(superM.items[0].id);
+console.log(superM.items[0].name);
+console.log(superM.items[0].price);
+
+console.log(superM.items[1].id);
+console.log(superM.items[1].name);
+console.log(superM.items[1].price);
+
+console.log(superM.items[2].id);
+console.log(superM.items[2].name);
+console.log(superM.items[2].price);
+
+
