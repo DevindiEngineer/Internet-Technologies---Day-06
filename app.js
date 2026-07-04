ff79bd3a3be9a9de512c47b3f341d5a2bd98afbf
DevindiEngineer <devindicampus@gmail.com>
2026-07-04 09:45:13 +0530

    OOP constructors

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+class Customer {
+    name;
+    age;
+    address;
 
-console.log(Hi);
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+
+let customer1 = new Customer("Dew", 20, "sri lanka");
+console.log(customer1);
+
+//===================================================
+
+let customer2 = { name: "Devindi", age: 20, address: "Colombo" };
+console.log(customer2);
